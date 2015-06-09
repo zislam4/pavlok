eb287ef6bf51baac0e8c6a9dc189a5a2f63f3a39
Zabir Islam <zabir.islam@tufts.edu>
2015-06-09 15:29:12 -0400

    Added request to server

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var main = function() {
 	$('.btn').click(function () {
 		console.log("You clicked on the button!");
+		var request = new XMLHttpRequest();
+		request.open('GET', 'pavlok-cal.herokuapp.com', true);
+		request.onreadystatechange(display);
+		request.send();
 	});
 }
 
+var display = function() {
+	
+	if (request.readystate == 4 && request.status == 200) {
+		console.log(request.responseText);
+
+	}
+}
 
 $(document).ready(main);
