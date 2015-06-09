b6b5e92eb2c467504142858f51d6533eae6e06c9
Zabir Islam <zabir.islam@tufts.edu>
2015-06-09 16:08:26 -0400

    Fixing url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var main = function() {
 
 	$('.btn').click(function () {
 		console.log("You clicked on the button!");
-		request.open('GET', 'pavlok-cal.herokuapp.com/addCal', true);
+		request.open('GET', 'https://pavlok-cal.herokuapp.com/', true);
 		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		request.onreadystatechange = function display() {
 			console.log(request.readyState);
