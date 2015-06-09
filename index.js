13bdfa2c2b1132cedfdb77849746a8d131e95e31
Zabir Islam <zabir.islam@tufts.edu>
2015-06-09 15:51:13 -0400

    Checking if data received

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ var main = function() {
 	$('.btn').click(function () {
 		console.log("You clicked on the button!");
 		request.open('GET', 'pavlok-cal.herokuapp.com', true);
-		request.onreadystatechange = 	function display() {
-		console.log(request.readyState);
-		if (request.readyState == 4 && request.status == 200) {
-			console.log("Request was successful");
-			console.log(request.response);
+		request.onreadystatechange = function display() {
+			console.log(request.readyState);
+			if (request.readyState ==) {
+				console.log(request.status);
+				if (request.status == 200) {
+					console.log("Request was successful");
+					console.log(request.response);
+				}
+			}
+		};
 
-		}
-	};
 		request.send();
 	});
 
