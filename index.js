278f166ae900c378eee86299976d13e9c6b2ab25
Zabir Islam <zabir.islam@tufts.edu>
2015-06-09 15:52:01 -0400

    Checking if data received

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var main = function() {
 		request.open('GET', 'pavlok-cal.herokuapp.com', true);
 		request.onreadystatechange = function display() {
 			console.log(request.readyState);
-			if (request.readyState ==) {
+			if (request.readyState == 4) {
 				console.log(request.status);
 				if (request.status == 200) {
 					console.log("Request was successful");
